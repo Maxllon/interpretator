9939d5384cc7acb540bfe47ea926e6673cf31a1d
Maksim <belkamax07@yandex.ru>
2024-05-03 20:34:25 +0300

    feat: Был полностью описан поток лексем в заголовочном файле

--- headers/token_list.h
+++ headers/token_list.h
@@ -3,4 +3,38 @@
 
 #include<includes.h>
 
+enum token_types
+{
+    KEYWORD,
+    STRING,
+    INTEGER,
+    DOUBLE
+};
+typedef union value
+{
+    long long int i;
+    long double f;
+    char* s;
+}value_t;
+typedef struct node
+{
+    node_t* next;
+    node_t* prev;
+
+    char* tk_type;
+    
+    value_t value;
+}node_t;
+
+node_t* new_node(char* tk_type, value_t value);
+void del_node(node_t* node);
+typedef struct tk_list
+{
+    node_t* father;
+
+    node_t* get();
+    node_t* previous();
+    node_t* next();
+}tk_list_t;
+
 #endif
