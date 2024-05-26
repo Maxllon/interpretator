25f0023f6c975471455eae9eea1714f9b42f3927
Maxllon <belkamax07@yandex.ru>
2024-05-26 18:43:19 +0300

    Заголовочный файл лексера

--- headers/lexer.h
+++ headers/lexer.h
@@ -3,4 +3,44 @@
 
 #include"includes.h"
 
+const wchar* KEYWORDS = L"если пока иначе вернуть не брать из функ нч кн";
+const wchar* STDFUNC = L"печать ввод промежуток";
+const wchar* BIN_OP = L"+ - = == * / // ^ != > < <= >= ост и или";
+const wchar* NUMBER = L"0123456789.";
+const wchar* SPEC = L";,()\"";
+
+typedef struct
+{
+    int x;
+    int y;
+}VEC_2;
+
+typedef enum
+{
+    END = 0,
+    KEYWORD,
+    VARIABLE,
+    STRING,
+    BINARY,
+    SPECIAL
+}TOKEN_KIND;
+
+typedef struct tk_node_t
+{
+    TOKEN_KIND kind;
+    wchar* value;
+    VEC_2 pos;
+
+    struct tk_node_t* next;
+    struct tk_node_t* previous;
+}tk_node;
+
+tk_node push_node(tk_node* main, tk_node* node);
+tk_node* new_node(TOKEN_KIND kind, wchar* value, VEC_2 pos);
+
+void delete_tk_list(tk_node* start_tk_list);
+
+tk_node* lexing(wchar* file);
+
+
 #endif
